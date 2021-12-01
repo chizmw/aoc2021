9378dc1049507ca42ece7dc6bfc0b13d8445bf60
Chisel <chisel.malik-wright@zoopla.co.uk>
2021-12-01 11:19:01 +0000

    Add solution for Day 1 Part 2

--- day-01/day01.py
+++ day-01/day01.py
@@ -13,8 +13,23 @@ def part1(data: List) -> int:
     return count
 
 
+def part2(data: List) -> int:
+    count = 0
+    prev_sum = None
+    for i, x in enumerate(data):
+        if i > 2:
+            prev_sum = sum(data[i - 4 : i - 1])
+            curr_sum = sum(data[i - 3 : i])
+
+            if curr_sum > prev_sum:
+                count += 1
+
+    return count
+
+
 with open("input.day1") as f:
     # strip those pesky newlines and make a list of the data
     # we read strings but want values, so convert to int
     data = [int(line.strip()) for line in f.readlines()]
     print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")
