2dd3a34a0faa269d6679d3872b110687f5e82522
Chisel <chisel.malik-wright@zoopla.co.uk>
2021-12-01 10:52:59 +0000

    Update how we calculate value for part 1

--- day-01/day01.py
+++ day-01/day01.py
@@ -2,16 +2,13 @@
 
 import sys
 
-# read from the file
 with open("input.day1") as f:
     # strip those pesky newlines and make a list of the data
     # we read strings but want values, so convert to int
     data = [int(line.strip()) for line in f.readlines()]
     count = 0
-    prev = None
-    for cur in data:
-        if prev is not None:
-            if cur > prev:
+    for i, x in enumerate(data):
+        if i > 0:
+            if data[i] > data[i - 1]:
                 count += 1
-        prev = cur
     print(count)
