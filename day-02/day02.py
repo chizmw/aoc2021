f7663cf16e4437d350c7c815e8c92e71a19ccbdd
Chisel <chisel.malik-wright@zoopla.co.uk>
2021-12-02 09:43:25 +0000

    Day 2 Part 2

--- day-02/day02.py
+++ day-02/day02.py
@@ -13,11 +13,29 @@ class Location:
         return f"H:{self.horizontal} D:{self.depth}"
 
 
+class Navigate:
+    def __init__(self, location: Location, aim: int = 0):
+        self.location: Location = location
+        self.aim: int = aim
+
+    def __str__(self) -> str:
+        return f"H:{self.horizontal} D:{self.depth}"
+
+    def forward(self, x: int):
+        self.location.horizontal += x
+        self.location.depth += self.aim * x
+
+    def down(self, x: int):
+        self.aim += x
+
+    def up(self, x: int):
+        self.aim -= x
+
+
 def follow_route(data: List, start: Location) -> Location:
     current = start
-    print(current)
     for item in data:
-        print(f"{item[0]} -> {item[1]}")
+        # print(f"{item[0]} -> {item[1]}")
         if item[0] == "forward":
             current.horizontal += item[1]
         elif item[0] == "down":
@@ -27,7 +45,19 @@ def follow_route(data: List, start: Location) -> Location:
 
     return current
 
-    print(1)
+
+def follow_route_with_aim(data: List, start: Location) -> Location:
+    current: Location = start
+    submarine = Navigate(current)
+    for item in data:
+        if item[0] == "forward":
+            submarine.forward(item[1])
+        elif item[0] == "down":
+            submarine.down(item[1])
+        elif item[0] == "up":
+            submarine.up(item[1])
+
+    return submarine.location
 
 
 with open("input.day2") as f:
@@ -38,6 +68,10 @@ with open("input.day2") as f:
     for item in data:
         item[1] = int(item[1])
 
-    location = Location()
-    location = follow_route(data, location)
+    # part 1
+    location = follow_route(data, Location())
+    print(f"{location}: {location.horizontal * location.depth}")
+
+    # part 2
+    location = follow_route_with_aim(data, Location())
     print(f"{location}: {location.horizontal * location.depth}")
