46b82028c59a125ab4f3f90a541438b53cec45ac
Chisel <chisel.malik-wright@zoopla.co.uk>
2021-12-02 09:28:17 +0000

    Day 2 Part 1

---
+++ day-02/day02.py
@@ -0,0 +1,43 @@
+#!python
+
+
+from typing import List
+
+
+class Location:
+    def __init__(self, horizontal: int = 0, depth: int = 0):
+        self.horizontal = horizontal
+        self.depth = depth
+
+    def __str__(self) -> str:
+        return f"H:{self.horizontal} D:{self.depth}"
+
+
+def follow_route(data: List, start: Location) -> Location:
+    current = start
+    print(current)
+    for item in data:
+        print(f"{item[0]} -> {item[1]}")
+        if item[0] == "forward":
+            current.horizontal += item[1]
+        elif item[0] == "down":
+            current.depth += item[1]
+        elif item[0] == "up":
+            current.depth -= item[1]
+
+    return current
+
+    print(1)
+
+
+with open("input.day2") as f:
+    # strip those pesky newlines and make a list of the data
+    # we're expecting a list of direction value pairs
+    data = [(line.strip().split()) for line in f.readlines()]
+    # we know the second item in each pair should be an int
+    for item in data:
+        item[1] = int(item[1])
+
+    location = Location()
+    location = follow_route(data, location)
+    print(f"{location}: {location.horizontal * location.depth}")
